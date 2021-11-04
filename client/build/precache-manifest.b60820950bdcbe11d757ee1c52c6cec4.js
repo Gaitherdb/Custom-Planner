913beb614446a0de5a51ece46331cc00026c0a33
Shelby <shelbylb97@comcast.net>
2021-11-03 20:22:09 -0400

    found code from an activity to refactor

---
+++ client/build/precache-manifest.b60820950bdcbe11d757ee1c52c6cec4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f536a8ff6921c2a02dcea198189b5bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26dc86c3e40430a71824",
+    "url": "/static/css/2.953aafb5.chunk.css"
+  },
+  {
+    "revision": "6ed0a039043bf89a6c98",
+    "url": "/static/css/main.850dc792.chunk.css"
+  },
+  {
+    "revision": "26dc86c3e40430a71824",
+    "url": "/static/js/2.8862c2a2.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.8862c2a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ed0a039043bf89a6c98",
+    "url": "/static/js/main.b050b9db.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
