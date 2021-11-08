404bb3dbe0d96fb2d2e86d161d1222f93ca34de8
David Gaither <gaitherdb@gmail.com>
2021-11-07 21:56:40 -0500

    add db.js. May not need these. Commented them out. for PWA

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,69 +1,69 @@
-const FILES_TO_CACHE = [
-    // './',
-    // `./index.js`,
-    // `./icons/icon-192x192.png`,
-    // `./icons/icon-512x512.png`,
-    // './styles.css',
-    // `https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css`
+// const FILES_TO_CACHE = [
+//     // './',
+//     // `./index.js`,
+//     // `./icons/icon-192x192.png`,
+//     // `./icons/icon-512x512.png`,
+//     // './styles.css',
+//     // `https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css`
   
   
-  ];
+//   ];
   
-  const PRECACHE = 'precache-v1';
-  const RUNTIME = 'runtime';
+//   const PRECACHE = 'precache-v1';
+//   const RUNTIME = 'runtime';
   
-  self.addEventListener('install', (event) => {
-    event.waitUntil(
+//   self.addEventListener('install', (event) => {
+//     event.waitUntil(
   
-      caches
-        .open(PRECACHE)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
-    );
-  });
+//       caches
+//         .open(PRECACHE)
+//         .then((cache) => cache.addAll(FILES_TO_CACHE))
+//         .then(self.skipWaiting())
+//     );
+//   });
   
-  // The activate handler takes care of cleaning up old caches.
-  self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-      caches
-        .keys()
-        .then((cacheNames) => {
-          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-        })
-        .then((cachesToDelete) => {
-          return Promise.all(
-            cachesToDelete.map((cacheToDelete) => {
-              return caches.delete(cacheToDelete);
-            })
-          );
-        })
-        .then(() => self.clients.claim())
-    );
-  });
+//   // The activate handler takes care of cleaning up old caches.
+//   self.addEventListener('activate', (event) => {
+//     const currentCaches = [PRECACHE, RUNTIME];
+//     event.waitUntil(
+//       caches
+//         .keys()
+//         .then((cacheNames) => {
+//           return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+//         })
+//         .then((cachesToDelete) => {
+//           return Promise.all(
+//             cachesToDelete.map((cacheToDelete) => {
+//               return caches.delete(cacheToDelete);
+//             })
+//           );
+//         })
+//         .then(() => self.clients.claim())
+//     );
+//   });
   
   
-  self.addEventListener('fetch', (event) => {
+//   self.addEventListener('fetch', (event) => {
     
-    if (event.request.method != `GET`) return;
+//     if (event.request.method != `GET`) return;
   
-    if (event.request.url.startsWith(self.location.origin)) {
-      event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
-          }
+//     if (event.request.url.startsWith(self.location.origin)) {
+//       event.respondWith(
+//         caches.match(event.request).then((cachedResponse) => {
+//           if (cachedResponse) {
+//             return cachedResponse;
+//           }
   
-          return caches.open(RUNTIME).then((cache) => {
-            return fetch(event.request).then((response) => {
-              return cache.put(event.request, response.clone()).then(() => {
-                return response;
-              });
-            });
-          });
-        })
-      );
-    }
-  });
+//           return caches.open(RUNTIME).then((cache) => {
+//             return fetch(event.request).then((response) => {
+//               return cache.put(event.request, response.clone()).then(() => {
+//                 return response;
+//               });
+//             });
+//           });
+//         })
+//       );
+//     }
+//   });
   
   
