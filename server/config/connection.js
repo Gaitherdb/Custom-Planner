fb6fe2070a82aae69ceb9d132811e6488bf3e12c
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-11-10 21:01:44 -0500

    Heroku deployment + MongoAtlas tether

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 console.log(process.env.DB_HOST)
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/afternoon-hamlet-16859', {
+mongoose.connect(process.env.MONGODB_URI || process.env.DB_HOST, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
