3b26fc96b564e5abb9c9f7fa9be66fa76a424986
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-11-10 20:52:56 -0500

    MONGODB Atlas + Heroku connect

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 console.log(process.env.DB_HOST)
-mongoose.connect(process.env.MONGODB_URI || process.env.DB_HOST, {
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/afternoon-hamlet-16859', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
