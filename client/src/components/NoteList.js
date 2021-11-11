4cc4376815a0d5c629301f2f74acdc138552f393
Shelby <shelbylb97@comcast.net>
2021-11-11 13:35:36 -0500

    moving css into app

--- client/src/components/NoteList.js
+++ client/src/components/NoteList.js
@@ -55,7 +55,7 @@ const NoteList = (props) => {
 
   return (
     <div>
-      <h3>What is on your note list?</h3>
+      <h3>What is on your plate for today?</h3>
       {todos &&
         todos.map((todo) => (
           <div className={
