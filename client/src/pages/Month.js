acc85dc258427fad5b74d850bd77a5e0a78476d3
David Gaither <gaitherdb@gmail.com>
2022-01-16 18:20:42 -0500

    todos render on month page, but isn't specific for month yet

--- client/src/pages/Month.js
+++ client/src/pages/Month.js
@@ -1,70 +1,58 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Container } from 'react-bootstrap';
-import { useHistory } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { GET_ME } from '../utils/queries';
-
-
-
+import React, { useState, useEffect, useRef } from "react";
+import { Container } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_ME } from "../utils/queries";
 
 const Month = (props) => {
-
-
     const { loading, data, refetch } = useQuery(GET_ME);
     const todos = data?.me || [];
-    var renderNoteList;
+    const thisMonthNotes = todos.savedTodos;
+    var renderMonthlyReview;
     var monthId;
 
-    console.log(todos)
+    console.log(thisMonthNotes);
 
-    // if (todos.savedTodos) {
-    //     renderNoteList = true;
+    if (todos.savedTodos) {
+        renderMonthlyReview = true;
 
-    //     if (!loading) {
-    //         // var thisMonthTodo = todos.savedTodos.filter(todo => todo.date === monthId)
-    //         //if they have ever posted before, they can see the notes section. We link part of the prop in the note section, and if they dont have todos it'll break
-
-    //     }
-    // }
-
-
-
-    if (!todos) {
-        return <h3>No notes yet</h3>;
+        if (!loading) {
+            // var thisMonthTodo = todos.savedTodos.filter(todo => todo.date === monthId)
+            //if they have ever posted before, they can see the notes section. We link part of the prop in the note section, and if they dont have todos it'll break
+        }
     }
 
+    if (!thisMonthNotes) {
+        return <h3>No notes for this month yet.</h3>;
+    }
 
-    return <h3>No notes yet</h3>;
-
-
-    // return (
-    //     <>
-    //         <Container className="wholeCon">
-    //             <div className="todolDiv text-center">
-    //                 {renderNoteList ? (
-    //                     <div className="col-12 col-md-8 mb-3">
-    //                         {loading ? (
-    //                             <div>Loading...</div>
-    //                         ) : (
-    //                             {
-    //                                 // todos.map((todo) => (
-    //                                 //     <div className='dark-color d-flex mb-1'
-    //                                 //         key={todo._id} >
-
-    //                                 //     </div>
-    //                                 // ))
-    //                             })};
-
-    //                     </div>
-    //                 ) : (<div>Add a note?</div>)};
-
-
-    //             </div>
-
-    //         </Container>
-    //     </>
-    // );
+    return (
+        <>
+            <Container className="wholeCon">
+                <div className="todolDiv text-center">
+                    {renderMonthlyReview ? (
+                        <div className="col-12 col-md-8 mb-3">
+                            {loading ? (
+                                <div>Loading...</div>
+                            ) : (
+                                <ol>
+                                    {thisMonthNotes.map((todo) => (
+                                        <li className=" d-flex mb-1 listItem" key={todo._id}>
+                                            {todo.task}
+                                        </li>
+                                    ))
+                                    }
+                                    </ol>
+                            )}
+                        </div>
+                    ) : (
+                        <div>Add a note to a date to see your monthly review!</div>
+                    )}
+                </div>
+            </Container>
+        </>
+    );
 };
 
 export default Month;
