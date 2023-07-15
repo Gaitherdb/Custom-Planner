06493b162fc0fdfd76446d3374c569367d287867
David Gaither <dgaither@instructors.2u.com>
2023-07-15 18:43:36 -0400

    add workbox serive worker- runs no errors

--- client/src/index.js
+++ client/src/index.js
@@ -11,4 +11,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-serviceWorkerRegistration.register(); //serviceWorker.js
+serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register(); 
--- client/src/serviceWorkerRegistration.js
+++ client/src/serviceWorkerRegistration.js
@@ -1,7 +1,7 @@
 export function register() {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/sw.js').then(registration => {
+            navigator.serviceWorker.register('../sw.js').then(registration => {
                 console.log('SW registered: ', registration);
             }).catch(registrationError => {
                 console.log('SW registration failed: ', registrationError);
@@ -9,7 +9,6 @@ export function register() {
         });
     }
 }
-
 
 export function unregister() {
     if ('serviceWorker' in navigator) {
@@ -22,4 +21,3 @@ export function unregister() {
             });
     }
 }
-
