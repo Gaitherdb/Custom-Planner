2a6da2557a20abfe811848e399a9d5e2166133cc
David Gaither <dgaither@instructors.2u.com>
2023-09-17 17:51:45 -0400

    add some boiler plate serviceWorkerReg functions

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import { precacheAndRoute } from 'workbox-precaching';
-import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate } from 'workbox-strategies';
+// import { precacheAndRoute } from 'workbox-precaching';
+// import { registerRoute } from 'workbox-routing';
+// import { StaleWhileRevalidate } from 'workbox-strategies';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,44 +15,45 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorkerRegistration.register();
-let deferredPrompt;
+serviceWorkerRegistration.register()
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update the UI notify the user they can add to home screen
-  showInstallPromotion();
-});
-function showInstallPromotion() {
-  // Display some UI to the user, like a button
-  // When user clicks on it, call prompt()
-  installButton.addEventListener('click', (e) => {
-    // Hide our user interface that shows our A2HS button
-    hideInstallPromotion();
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
-    });
-  });
-}
+// let deferredPrompt;
+
+// window.addEventListener('beforeinstallprompt', (e) => {
+//   // Prevent Chrome 67 and earlier from automatically showing the prompt
+//   e.preventDefault();
+//   // Stash the event so it can be triggered later.
+//   deferredPrompt = e;
+//   // Update the UI notify the user they can add to home screen
+//   showInstallPromotion();
+// });
+// function showInstallPromotion() {
+//   // Display some UI to the user, like a button
+//   // When user clicks on it, call prompt()
+//   installButton.addEventListener('click', (e) => {
+//     // Hide our user interface that shows our A2HS button
+//     hideInstallPromotion();
+//     // Show the prompt
+//     deferredPrompt.prompt();
+//     // Wait for the user to respond to the prompt
+//     deferredPrompt.userChoice.then((choiceResult) => {
+//       if (choiceResult.outcome === 'accepted') {
+//         console.log('User accepted the A2HS prompt');
+//       } else {
+//         console.log('User dismissed the A2HS prompt');
+//       }
+//       deferredPrompt = null;
+//     });
+//   });
+// }
 
 
 
-// Precache assets
-precacheAndRoute(self.__WB_MANIFEST);
+// // Precache assets
+// precacheAndRoute(self.__WB_MANIFEST);
 
-// Use stale-while-revalidate strategy for other assets
-registerRoute(
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new StaleWhileRevalidate()
-);
+// // Use stale-while-revalidate strategy for other assets
+// registerRoute(
+//   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+//   new StaleWhileRevalidate()
+// );
--- client/src/serviceWorkerRegistration.js
+++ client/src/serviceWorkerRegistration.js
@@ -10,3 +10,71 @@
 //     }
 // }
 
+const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+    window.location.hostname === '[::1]' ||
+    window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+console.log(isLocalhost, "isLocalhost")
+export function register(config) {
+    // Ensure service workers are supported
+    if ('serviceWorker' in navigator) {
+        // Ensure the page is served over HTTPS or localhost
+        if (process.env.NODE_ENV === 'production' || isLocalhost) {
+            const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+            if (publicUrl.origin !== window.location.origin) {
+                return;
+            }
+
+            window.addEventListener('load', () => {
+                const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+                if (isLocalhost) {
+                    checkValidServiceWorker(swUrl, config);
+                } else {
+                    registerValidSW(swUrl, config);
+                }
+            });
+        }
+    }
+}
+
+function registerValidSW(swUrl, config) {
+    navigator.serviceWorker
+        .register(swUrl)
+        .then((registration) => {
+            registration.onupdatefound = () => {
+                // ... (code to handle updates to the service worker)
+            };
+        })
+        .catch((error) => {
+            console.error('Error during service worker registration:', error);
+        });
+}
+
+function checkValidServiceWorker(swUrl, config) {
+    fetch(swUrl, {
+        headers: { 'Service-Worker': 'script' },
+    })
+        .then((response) => {
+            console.log("checkValid?")
+            // ... (code to check if the service worker is valid or needs to be updated)
+        })
+        .catch(() => {
+            console.log('No internet connection. App is running in offline mode.');
+        });
+}
+
+export function unregister() {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.ready
+            .then((registration) => {
+                registration.unregister();
+            })
+            .catch((error) => {
+                console.error(error.message);
+            });
+    }
+}
